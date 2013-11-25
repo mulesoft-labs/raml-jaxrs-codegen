f52901b0bfad51c236fadb01cc814a57763c8abc
David Dossot <david@dossot.net>
2013-11-25 13:57:50 -0800

    simplified codemodel usage

--- core/src/main/java/org/raml/jaxrs/codegen/core/model/CustomHttpMethod.java
+++ core/src/main/java/org/raml/jaxrs/codegen/core/model/CustomHttpMethod.java
@@ -11,10 +11,8 @@ import javax.ws.rs.HttpMethod;
 
 import org.apache.commons.lang.Validate;
 
-import com.sun.codemodel.JAnnotationUse;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
-import com.sun.codemodel.JDocComment;
 import com.sun.codemodel.JPackage;
 
 public class CustomHttpMethod implements JavaRenderable
@@ -28,21 +26,18 @@ public class CustomHttpMethod implements JavaRenderable
         httpMethodName = name.toUpperCase();
     }
 
+    @Override
     public void toJava(final File outputDirectory) throws Exception
     {
         final JCodeModel codeModel = new JCodeModel();
         final JPackage pkg = codeModel._package(JavaRenderable.SUPPORT_CLASSES_PACKAGE);
         final JDefinedClass clazz = pkg._annotationTypeDeclaration(httpMethodName);
 
-        JAnnotationUse annotation = clazz.annotate(Target.class);
-        annotation.param("value", ElementType.METHOD);
-        annotation = clazz.annotate(Retention.class);
-        annotation.param("value", RetentionPolicy.RUNTIME);
-        annotation = clazz.annotate(HttpMethod.class);
-        annotation.param("value", httpMethodName);
+        clazz.annotate(Target.class).param("value", ElementType.METHOD);
+        clazz.annotate(Retention.class).param("value", RetentionPolicy.RUNTIME);
+        clazz.annotate(HttpMethod.class).param("value", httpMethodName);
 
-        final JDocComment jDocComment = clazz.javadoc();
-        jDocComment.add("Custom JAX-RS support for HTTP " + httpMethodName + ".");
+        clazz.javadoc().add("Custom JAX-RS support for HTTP " + httpMethodName + ".");
 
         codeModel.build(outputDirectory);
     }
