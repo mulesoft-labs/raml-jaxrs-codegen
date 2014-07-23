af57389146ff3033b9320936412428e0dea920de
KonstantinSviridov <ksviridov@gmail.com>
2014-07-24 01:18:55 +0700

    Adding a default raml sourceDirectory of src/main/raml

--- maven-plugin/src/main/java/org/raml/jaxrs/codegen/maven/RamlJaxrsCodegenMojo.java
+++ maven-plugin/src/main/java/org/raml/jaxrs/codegen/maven/RamlJaxrsCodegenMojo.java
@@ -68,7 +68,7 @@ public class RamlJaxrsCodegenMojo extends AbstractMojo
     /**
      * Directory location of the RAML file(s).
      */
-    @Parameter(property = "sourceDirectory")
+    @Parameter(property = "sourceDirectory", defaultValue = "${basedir}/src/main/raml")
     private File sourceDirectory;
 
     /**
@@ -176,7 +176,13 @@ public class RamlJaxrsCodegenMojo extends AbstractMojo
 
     private Collection<File> getRamlFiles() throws MojoExecutionException
     {
-        if (sourceDirectory != null)
+				if (sourcePaths != null && sourcePaths.length > 0 )
+				{
+            final List<File> sourceFiles = Arrays.asList(sourcePaths);
+            getLog().info("Using RAML files: " + sourceFiles);
+            return sourceFiles;
+				}
+				else
         {
             if (!sourceDirectory.isDirectory())
             {
@@ -187,12 +193,6 @@ public class RamlJaxrsCodegenMojo extends AbstractMojo
             getLog().info("Looking for RAML files in and below: " + sourceDirectory);
 
             return FileUtils.listFiles(sourceDirectory, new String[]{"raml", "yaml"}, true);
-        }
-        else
-        {
-            final List<File> sourceFiles = Arrays.asList(sourcePaths);
-            getLog().info("Using RAML files: " + sourceFiles);
-            return sourceFiles;
         }
     }
 }
